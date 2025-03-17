a3b81d25f7d528799ffa8512e0901e2fff004b9d
SHANE <sam6091260@gmail.com>
2025-03-17 19:19:58 +0800

    docs: svg to png

--- src/Helmet.jsx
+++ src/Helmet.jsx
@@ -9,31 +9,22 @@ const MyHelmet = () => {
       <meta name="description" content="ROUGE COCO" />
       <meta property="og:title" content="ROUGE COCO" />
       <meta property="og:description" content="探索專屬您的COCO唇膏色選" />
-      <meta
-        property="og:image"
-        content="https://popevent.netlify.app/images/coco.png?v=1"
-      />
+      <meta property="og:image" content="https://i.imgur.com/5EcoKFR.png" />
 
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="ROUGE COCO" />
       <meta name="twitter:description" content="探索專屬您的COCO唇膏色選" />
-      <meta
-        name="twitter:image"
-        content="https://popevent.netlify.app/images/coco.png?v=1"
-      />
+      <meta name="twitter:image" content="https://i.imgur.com/5EcoKFR.png" />
 
       <link
         rel="icon"
         type="image/png"
-        href="https://popevent.netlify.app/images/coco.png?v=1"
+        href="https://i.imgur.com/5EcoKFR.png"
         sizes="32x32"
       />
-      <link
-        rel="shortcut icon"
-        href="https://popevent.netlify.app/images/coco.png?v=1"
-      />
+      <link rel="shortcut icon" href="https://i.imgur.com/5EcoKFR.png" />
     </Helmet>
   );
 };
