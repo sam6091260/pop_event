8551f72eba77c33bb8922e5f6791a3ce670cc7ca
SHANE <sam6091260@gmail.com>
2025-03-17 23:43:14 +0800

    docs: svg to png

--- src/Helmet.jsx
+++ src/Helmet.jsx
@@ -1,7 +1,8 @@
 import { Helmet } from "react-helmet";
 
 const MyHelmet = () => {
-  const PIC = "https://popevent.netlify.app/images/coco.png?v=2"; // 確保圖片網址正確
+  const PIC =
+    "https://popevent.netlify.app/images/coco.png?v=" + new Date().getTime();
 
   return (
     <Helmet>
@@ -10,8 +11,8 @@ const MyHelmet = () => {
       <meta property="og:title" content="ROUGE COCO" />
       <meta property="og:description" content="探索專屬您的COCO唇膏色選" />
       <meta property="og:image" content={PIC} />
-      <meta property="og:image:width" content="1000" />
-      <meta property="og:image:height" content="580" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://popevent.netlify.app/" />
       <meta name="twitter:card" content="summary_large_image" />
